819f40c0ec0778f56b3c490987e0466d9267ac92
Pedro Monteiro <pedro.monteiro.ps@gmail.com>
2026-09-15 23:50:45 +0100

    fix: type email MFA setup translations

--- src/Security/EmailAuthentication/Actions/SetUpEmailAuthenticationAction.php
+++ src/Security/EmailAuthentication/Actions/SetUpEmailAuthenticationAction.php
@@ -24,7 +24,7 @@ class SetUpEmailAuthenticationAction
     public static function make(EmailAuthentication $emailAuthentication): Action
     {
         return Action::make('setUpEmailAuthentication')
-            ->label(__('filament-panels::auth/multi-factor/email/actions/set-up.label'))
+            ->label(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.label'))
             ->color('primary')
             ->icon(FilamentIcon::resolve(PanelsIconAlias::AUTH_MULTI_FACTOR_EMAIL_ACTIONS_SET_UP) ?? Heroicon::LockClosed)
             ->link()
@@ -39,25 +39,25 @@ class SetUpEmailAuthenticationAction
             ->modalWidth(Width::Large)
             ->modalIcon(FilamentIcon::resolve(PanelsIconAlias::AUTH_MULTI_FACTOR_EMAIL_ACTIONS_SET_UP_MODAL) ?? Heroicon::OutlinedLockClosed)
             ->modalIconColor('primary')
-            ->modalHeading(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.heading'))
-            ->modalDescription(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.description'))
+            ->modalHeading(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.heading'))
+            ->modalDescription(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.description'))
             ->schema([
                 OneTimeCodeInput::make('code')
-                    ->label(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.label'))
+                    ->label(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.label'))
                     ->belowContent(function () use ($emailAuthentication): Action {
                         /** @var HasEmailAuthentication $user */
                         $user = Filament::auth()->user();
 
                         return $emailAuthentication->makeResendAction($user);
                     })
-                    ->validationAttribute(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.validation_attribute'))
+                    ->validationAttribute(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.validation_attribute'))
                     ->required()
                     ->rule(function () use ($emailAuthentication): Closure {
                         return function (string $attribute, #[SensitiveParameter] $value, Closure $fail) use ($emailAuthentication): void {
                             $rateLimitingKey = 'filament-set-up-email-authentication:'.Filament::auth()->id();
 
                             if (RateLimiter::tooManyAttempts($rateLimitingKey, maxAttempts: 5)) {
-                                $fail(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.messages.rate_limited'));
+                                $fail(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.messages.rate_limited'));
 
                                 return;
                             }
@@ -68,12 +68,12 @@ class SetUpEmailAuthenticationAction
                                 return;
                             }
 
-                            $fail(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.messages.invalid'));
+                            $fail(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.form.code.messages.invalid'));
                         };
                     }),
             ])
             ->modalSubmitAction(fn (Action $action) => $action
-                ->label(__('filament-panels::auth/multi-factor/email/actions/set-up.modal.actions.submit.label')))
+                ->label(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.modal.actions.submit.label')))
             ->action(function (): void {
                 /** @var Authenticatable&HasEmailAuthentication $user */
                 $user = Filament::auth()->user();
@@ -83,11 +83,18 @@ class SetUpEmailAuthenticationAction
                 });
 
                 Notification::make()
-                    ->title(__('filament-panels::auth/multi-factor/email/actions/set-up.notifications.enabled.title'))
+                    ->title(self::translate('filament-panels::auth/multi-factor/email/actions/set-up.notifications.enabled.title'))
                     ->success()
                     ->icon(FilamentIcon::resolve(PanelsIconAlias::AUTH_MULTI_FACTOR_EMAIL_ACTIONS_SET_UP_NOTIFICATION) ?? Heroicon::OutlinedLockClosed)
                     ->send();
             })
             ->rateLimit(5);
+    }
+
+    protected static function translate(string $key): string
+    {
+        $translation = __($key);
+
+        return is_string($translation) ? $translation : $key;
     }
 }
