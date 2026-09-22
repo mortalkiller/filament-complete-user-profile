067e06a8106813e435113d4a6ea191415c6f4b95
Pedro Monteiro <pedro.monteiro.ps@gmail.com>
2026-09-22 09:04:47 +0100

    Showcase extension points in workbench

--- workbench/app/Models/DemoUser.php
+++ workbench/app/Models/DemoUser.php
@@ -9,6 +9,7 @@ use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasAvatar;
 use Filament\Panel;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Mortalkiller\FilamentCompleteUserProfile\Concerns\InteractsWithEmailAuthentication;
@@ -30,6 +31,12 @@ class DemoUser extends Authenticatable implements FilamentUser, HasAvatar, HasEm
         'password',
         'remember_token',
     ];
+
+    /** @return HasMany<DemoAddress, $this> */
+    public function addresses(): HasMany
+    {
+        return $this->hasMany(DemoAddress::class, 'user_id');
+    }
 
     public function canAccessPanel(Panel $panel): bool
     {
